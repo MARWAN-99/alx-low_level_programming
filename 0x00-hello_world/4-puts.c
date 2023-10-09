dcc66a95c531b9b03b4b6697173b601c8291cc53
Mar_wan <marwanboudraforever2199@gmail.com>
2023-10-09 15:30:31 -0700

    new correction 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *prints exactly "Programming is like building a multilingual puzzle"
- *main - entry poit
+ *using puts function
+ *by puts
+ *The main function is the entry point
  *return: always 0 (success)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
