8da2e7801edeb656a9a5896f299e68e8c002c612
marwan-99 <marwanboudraforever2199@gmail.com>
2024-01-01 13:44:11 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * add - function that add two integers
+ * @n1: the firstr number to be comuter
+ * @n2; the second number to be computer
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
+
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,7 @@
+#include "main.c"
+
+/**
+ * time_table - write the 9 time table, statring with 0
+ * Return: empty output
+ */
+
