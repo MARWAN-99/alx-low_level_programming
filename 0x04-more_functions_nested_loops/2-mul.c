fbbf22bd4ef4ffcb5fda6fe3fe2c8b20dafb10d5
marwan-99 <marwanboudraforever2199@gmail.com>
2024-01-05 13:38:08 -0800

    correction Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * mul - function that multiplies tow integers 
+ * mul - function that multiplies two integers 
  * @n1: the first number to be computer
  * @n2: the second number to be computer
  * Return: integer value
