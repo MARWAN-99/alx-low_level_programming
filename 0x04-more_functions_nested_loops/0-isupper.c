05e04c89cb21660e74583e0e456a6ebf7799d68f
marwan-99 <marwanboudraforever2199@gmail.com>
2024-01-05 12:27:32 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks uppercase character
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+{
+return (0);
+}
+}
